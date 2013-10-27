f4ce23eda269a0820636b3f4207547d6509d58f7
ampers4nd <eric.schow@gmail.com>
2013-10-26 22:24:09 -0700

    * made the reload script a little bit smarter (not too smart, though)

--- reloadBang.sh
+++ reloadBang.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-yaws --load bang
-yaws --load bang_utilities
-yaws --load bang_user
-yaws --load bang_db
-yaws --load bang_json
-yaws --load bang_http
+PWD=`pwd`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${DIR}/src
+for erl in `ls *.erl`; do
+    erlc $erl
+    filename="${erl%.*}"
+    mv ${filename}.beam ../ebin/
+    yaws --load ${filename}
+done
+cd $PWD
+
+#yaws --load bang
+#yaws --load bang_utilities
+#yaws --load bang_user
+#yaws --load bang_db
+#yaws --load bang_json
+#yaws --load bang_http
