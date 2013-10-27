86da9d1a86c8d46f0c11cdb361c3f7f0f7ea22dc
ampers4nd <eric.schow@gmail.com>
2013-10-27 10:25:46 -0700

    * returning 400 from PUT with bad parameters

--- src/bang_db.erl
+++ src/bang_db.erl
@@ -54,8 +54,37 @@ doInsert(Body) ->
 	end.
 
 doUpdate(Body) ->
-	{ok, UID} = rfc4627:get_field(Body, "val_id"),
-	{ok, UserType} = rfc4627:get_field(Body, "user_type"),
+	doUpdate(rfc4627:get_field(Body, "val_id"), rfc4627:get_field(Body, "user_type")).
+	% {ok, UID} = rfc4627:get_field(Body, "val_id"),
+	% {ok, UserType} = rfc4627:get_field(Body, "user_type"),
+	% URL = baseURL() ++ binary_to_list(UID),
+	% {ok, {{_Version, GETResponseCode, _ReasonPhrase}, _Headers, EncodedJSONOld}} = bang_http:get(URL), 
+	% case GETResponseCode of
+	% 	200 ->
+	% 		{ok, DecodedJSONOld, _} = rfc4627:decode(list_to_binary(EncodedJSONOld)),
+	% 		DecodedJSONNew = rfc4627:set_field(DecodedJSONOld, "user_type", UserType),
+	% 		PUTRequest = bang_http:put(URL, rfc4627:encode(DecodedJSONNew)),
+	% 		{ok, {{_Version2, PUTResponseCode, _ReasonPhrase2}, _Headers2, PUTResponseBody}} = PUTRequest,
+	% 		case PUTResponseCode of
+	% 			201 ->
+	% 				{ok, JSONResponse, _} = rfc4627:decode(list_to_binary(PUTResponseBody)),
+	% 				error_logger:info_msg("JSON Response: ~p~n", [JSONResponse]), 
+	% 				Record = {obj, [{"success", <<"true">>},
+	% 								{"val_id", UID},
+	% 								{"user_type", UserType}]},
+	% 				Response = rfc4627:encode(Record),
+	% 				[{html, Response},
+	% 				 bang_json:contentHeader(),
+	% 				 {status, PUTResponseCode}];
+	% 			_ ->
+	% 				{status, PUTResponseCode}
+	% 		end;
+	% 	_ ->
+	% 		{status, GETResponseCode}
+	% end.
+
+
+doUpdate({ok, UID}, {ok, UserType}) ->
 	URL = baseURL() ++ binary_to_list(UID),
 	{ok, {{_Version, GETResponseCode, _ReasonPhrase}, _Headers, EncodedJSONOld}} = bang_http:get(URL), 
 	case GETResponseCode of
@@ -80,4 +109,6 @@ doUpdate(Body) ->
 			end;
 		_ ->
 			{status, GETResponseCode}
-	end.
+	end;
+doUpdate(_, _) ->
+	{status, 400}.
