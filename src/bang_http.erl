d858a1fd5381787392f72a52d79033975bff5012
ampers4nd <eric.schow@gmail.com>
2013-10-26 17:47:38 -0700

    * added bang_http module

---
+++ src/bang_http.erl
@@ -0,0 +1,41 @@
+-module(bang_http).
+
+-export([get/1, get/3, post/2, post/4]).
+
+auth_header(User, PW) ->
+    Encoded = base64:encode_to_string(lists:append([User,":",PW])),
+    {"Authorization", "Basic " ++ Encoded}.
+
+headers(json) -> [{"Content-Type", bang_json:contentType()}].
+headers(User, PW) -> [auth_header(User, PW)].
+headers(User, PW, json) -> [auth_header(User, PW), {"Content-Type", bang_json:contentType()}].
+
+% options() -> [{sync, true}].
+options() -> [].
+httpOptions() -> [].
+
+get(URL) ->
+	httpc:request(get, {URL, []}, httpOptions(), options()).
+
+get(User, PW, URL) ->
+	request(get, User, PW, URL, []).
+
+post(URL, Body) ->
+	httpc:request(post, 
+					{URL, headers(json), bang_json:contentType(), Body},
+		 			httpOptions(), 
+		 			options()). 
+
+post(User, PW, URL, Body) ->
+	request(post, User, PW, URL, Body).
+
+request(get, User, PW, URL, _Body) ->
+	httpc:request(get, 
+					{URL, headers(User, PW)}, 
+					httpOptions(), 
+					options());
+request(post, User, PW, URL, Body) ->
+	httpc:request(post, 
+					{URL, headers(User, PW, json), bang_json:contentType(), Body},
+ 					httpOptions(), 
+ 					options()).
